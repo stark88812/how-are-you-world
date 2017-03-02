eaaf20cb962d67f36d59d97195863d4ffcc25c5b
stark88812 <aswajith.jithu777@gmail.com>
2017-03-02 12:26:18 +0530

    Add files via upload

---
+++ trogen1.sh
@@ -0,0 +1,20 @@
+cdeject()
+{
+echo  "\a\a\a\a\a\a\a"
+eject
+
+sleep 1
+echo  "\a\a\a\a\a\a\a"
+eject -t
+}
+echo
+echo  "your system is affected by a \033[31m\033[5mcd-rom virus\033[0m\c"
+cdeject
+cdeject
+cdeject
+~
+~
+~
+~
+~
+~ 
